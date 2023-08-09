72386d99c14e6e2f26d4c2dfce1329545aa58085
wallds <cqc604115985@live.com>
2023-08-09 19:53:23 +0800

    Update directive.hpp

--- src/symex/directive.hpp
+++ src/symex/directive.hpp
@@ -58,20 +58,145 @@ namespace vtil::python
 			: class_( scope, name )
 			{
 				( *this )
+					// Constructor
+					//
+					//.def( py::init([](const char* id, int lookup_index, directive::matching_type mtype) {
+					//		return directive::instance((new std::string(id))->c_str(), lookup_index, mtype);
+					//	}), py::arg("id"), py::arg("lookup_index"), py::arg("mtype") = directive::match_any )
+
+					// Functions
+					//
 					.def( "to_string", &directive::instance::to_string )
 					.def( "__repr__", &directive::instance::to_string )
 					.def( "__str__", &directive::instance::to_string )
-					//.def( py::self + py::self )
-					.def( py::self - py::self )
+
+					.def( "__invert__", [](const directive::instance& rhs) { return ~rhs; } )
+					.def( "__neg__", [](const directive::instance& rhs) { return -rhs; } )
+					// .def( "__pos__", [](const directive::instance& rhs) { return rhs; } )
+					.def( "popcnt", [](const directive::instance& rhs) { return __popcnt(rhs); } )
+					.def( "bsf", [](const directive::instance& rhs) { return __bsf(rhs); } )
+					.def( "bsr", [](const directive::instance& rhs) { return __bsr(rhs); } )
+					.def( "mask", [](const directive::instance& rhs) { return __mask(rhs); } )
+					.def( "bcnt", [](const directive::instance& rhs) { return __bcnt(rhs); } )
+
+					// vs2022 Error C2678
+					// .def( py::self + py::self )
+					// .def( py::self - py::self )
 					// .def( py::self | py::self )
 					// .def( py::self & py::self )
 					// .def( py::self ^ py::self )
-					.def( py::self * py::self )
-					.def( py::self * int64_t() )
+					// .def( py::self * py::self )
+					// .def( py::self * int64_t() )
 					// .def( py::self / py::self )
 					// .def( py::self % py::self )
-
+					
+					// End
+					//
 					;
+
+#define IMPLEMENT_OPERATOR( name, ... ) 																				\
+		.def( name, [ ] ( const directive::instance& lhs, const directive::instance& rhs ) { return __VA_ARGS__; } )	\
+		.def( name, [ ] ( const directive::instance& lhs, int64_t rhs ) { return __VA_ARGS__; } )						\
+		.def( name, [ ] ( const directive::instance& lhs, uint64_t rhs ) { return __VA_ARGS__; } )
+					
+				( *this )
+					// Functions
+					//
+					IMPLEMENT_OPERATOR( "rotr", __rotr(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "rotl", __rotl(lhs, rhs) )
+
+					IMPLEMENT_OPERATOR( "mulhi", mulhi(lhs, rhs))
+					IMPLEMENT_OPERATOR( "umulhi", umulhi(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "umul", umul(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "udiv", udiv(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "urem", urem(lhs, rhs) )
+
+					IMPLEMENT_OPERATOR( "ucast", __ucast(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "cast", __cast(lhs, rhs) )
+
+					IMPLEMENT_OPERATOR( "bt", __bt(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "if_", __if(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "max", __max(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "min", __min(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "umax", __umax(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "umin", __umin(lhs, rhs) )
+
+					IMPLEMENT_OPERATOR( "ugreat", __ugreat(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "ugreat_eq", __ugreat_eq(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "uequal", __uequal(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "unot_equal", __unot_equal(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "uless_eq", __uless_eq(lhs, rhs) )
+					IMPLEMENT_OPERATOR( "uless", __uless(lhs, rhs) )
+
+					IMPLEMENT_OPERATOR( "__add__", lhs + rhs )
+					IMPLEMENT_OPERATOR( "__sub__", lhs - rhs )
+					IMPLEMENT_OPERATOR( "__mul__", lhs * rhs )
+					IMPLEMENT_OPERATOR( "__truediv__", lhs / rhs )
+					IMPLEMENT_OPERATOR( "__mod__", lhs % rhs )
+					IMPLEMENT_OPERATOR( "__and__", lhs & rhs )
+					IMPLEMENT_OPERATOR( "__or__", lhs | rhs )
+					IMPLEMENT_OPERATOR( "__xor__", lhs ^ rhs )
+					IMPLEMENT_OPERATOR( "__lshift__", lhs << rhs )
+					IMPLEMENT_OPERATOR( "__rshift__", lhs >> rhs )
+					IMPLEMENT_OPERATOR( "__gt__", lhs > rhs )
+					IMPLEMENT_OPERATOR( "__ge__", lhs >= rhs )
+					IMPLEMENT_OPERATOR( "__lt__", lhs < rhs )
+					IMPLEMENT_OPERATOR( "__le__", lhs <= rhs )
+					IMPLEMENT_OPERATOR( "__eq__", lhs == rhs )
+					IMPLEMENT_OPERATOR( "__ne__", lhs != rhs )
+					
+					// End
+					//
+					;
+#undef IMPLEMENT_OPERATOR
+
+//#define IMPLEMENT_IOPERATOR( name, ... ) 																		\
+//		.def( name, [ ] ( directive::instance& lhs, const directive::instance& rhs ) { return __VA_ARGS__; } )	\
+//		.def( name, [ ] ( directive::instance& lhs, int64_t rhs ) { return __VA_ARGS__; } )						\
+//		.def( name, [ ] ( directive::instance& lhs, uint64_t rhs ) { return __VA_ARGS__; } )
+//
+//				(*this)
+//					// Functions
+//					//
+//					IMPLEMENT_IOPERATOR( "__iadd__", lhs += rhs )
+//					IMPLEMENT_IOPERATOR( "__isub__", lhs -= rhs )
+//					IMPLEMENT_IOPERATOR( "__imul__", lhs *= rhs )
+//					IMPLEMENT_IOPERATOR( "__itruediv__", lhs /= rhs )
+//					IMPLEMENT_IOPERATOR( "__imod__", lhs %= rhs )
+//					IMPLEMENT_IOPERATOR( "__ilshift__", lhs <<= rhs )
+//					IMPLEMENT_IOPERATOR( "__irshift__", lhs >>= rhs )
+//					IMPLEMENT_IOPERATOR( "__iand__", lhs &= rhs )
+//					IMPLEMENT_IOPERATOR( "__ixor__", lhs ^= rhs )
+//					IMPLEMENT_IOPERATOR( "__ior__", lhs |= rhs )
+//
+//					// End
+//					//
+//					;
+//#undef IMPLEMENT_IOPERATOR
+
+
+#define IMPLEMENT_ROPERATOR( name, ... ) 															\
+		.def( name, [ ] ( const directive::instance& rhs, int64_t lhs ) { return __VA_ARGS__; } )	\
+		.def( name, [ ] ( const directive::instance& rhs, uint64_t lhs ) { return __VA_ARGS__; } )
+
+				( *this )
+					// Functions
+					//
+					IMPLEMENT_ROPERATOR( "__radd__", lhs + rhs )
+					IMPLEMENT_ROPERATOR( "__rsub__", lhs - rhs )
+					IMPLEMENT_ROPERATOR( "__rmul__", lhs * rhs )
+					IMPLEMENT_ROPERATOR( "__rtruediv__", lhs / rhs )
+					IMPLEMENT_ROPERATOR( "__rmod__", lhs % rhs )
+					IMPLEMENT_ROPERATOR( "__rand__", lhs & rhs )
+					IMPLEMENT_ROPERATOR( "__ror__", lhs | rhs )
+					IMPLEMENT_ROPERATOR( "__rxor__", lhs ^ rhs )
+					IMPLEMENT_ROPERATOR( "__rlshift__", lhs << rhs )
+					IMPLEMENT_ROPERATOR( "__rrshift__", lhs >> rhs )
+
+					// End
+					//
+					;
+#undef IMPLEMENT_ROPERATOR
 			}
 		};
 
